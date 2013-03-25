81bcffecdada596847527e68072ef205a5151d6b
M Fuad Adib <mfuadadib@yahoo.com>
2013-03-25 11:44:57 +0700

    Apotik File Awal

---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>APOTIKUIN</title>
+<link href="styles/style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<div id="mainPan">
+  <div id="topPan">
+  	<a href="index.html"><img src="images/logo.gif" alt="Expart Vision" width="230" height="44" border="0" class="logo" /></a> 
+		<ul>
+			<li><a href="#">Home</a></li>
+			<li class="about"><a href="#">About Us</a></li>
+			<li><a href="#">Berita</a></li>
+			<li><a href="#">Login</a></li>
+		</ul>
+  </div> <?php /*
+  <div id="toprightPan">
+  	<p></p>
+  </div>
+ 
+  <div id="bodyTopPan">
+  	<p></p>
+	<p class="more"><a href="#">more</a></p>
+  </div>
+  */?>
+  <div id="leftPan">
+	<h2>on 1st, england<br />
+	<span>M piterson</span></h2>
+	<p><span>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec Class aptent taciti lectarcu egestas luctus.</span></p>
+	<p>d nulla quis tellus ornare aliquam. Curabitur eu lectus eu massa orni mperdiet. Proin nec neque vitae lacus sollicitudin malesuada. Curab vitae magna. metus non blandit aliquet,dalacut molestie arcu ante at felis.</p>
+	<div id="bodyleftlinkboxonePan">
+			<ul>
+			<li class="comments">Comments: <span class="boldtext">23</span></li>
+			<li class="continue"><a href="#">Continue reading....</a></li>
+			</ul>
+	</div>
+	<p class="bottompadding">&nbsp;</p>
+  </div>
+  
+  <div id="bodyrightPan">
+  	<h2>publications</h2>
+	<p>metus non blandit aliquet, massa dui sodales lacus, ut molestie arcu</p>
+	<p><span>24th may 2006</span></p>
+	<p class="toppadding-rightpantext">ante at felis.Duis vestibulum, ipsum id cursus pellent esq ue, eros leo viverra est,vel posuere felis mauris </p>
+	<p><span>26th may 2006</span></p>
+  </div>
+</div>
+
+<div id="footermainPan">
+	<div id="footerPan">
+  <ul>
+  	<li><a href="#">Home </a>| </li>
+  	<li><a href="#">About Us</a> | </li>
+  	<li><a href="#">Login </a> </li>
+  </ul>
+  
+  <ul class="info">
+  	<li class="address">23-Ambush road 0128thonhill <br />CANADA</li>
+	<li class="phone"><span>PH: </span><br />
+	  012-2568-6897 <br />
+	  012-2568-6898</li>
+	 <li class="fax"><span>FAX:</span> <br />
+	  012-2568-6897 <br />
+	  012-2568-6898</li>
+    </ul>
+   <p class="copyright">©expartvision. All right reserved.</p>
+   <ul class="templateworld">
+  	<li>Design By:</li>
+	<li><a href="http://www.templateworld.com" target="_blank">Template World</a></li>
+  </ul>
+  </div>
+</div>
+</body>
+</html>
